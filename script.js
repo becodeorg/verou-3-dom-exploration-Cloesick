63e2713a389ddd3ac924006042def39f19b841d7
Nicolas <93992407+Cloesick@users.noreply.github.com>
2022-01-12 15:44:58 +0100

    card info added

---
+++ script.js
@@ -0,0 +1,115 @@
+const gameOfTheYear = [
+	{
+		name: "Resident Evil Village",
+		creator: "Quentin Tarantino",
+		releaseYear: 2021,
+		picture: "link/to/a/picture",
+		genre: ["survival-horror"],
+	},
+
+	// ...
+]```
+const gameOfTheYear = [
+  {
+    name: 'Cyberpunk 2077',
+    creator: 'CD Projekt',
+    releaseYear: 2020,
+    picture: 'link/to/a/picture',
+    genre: ['open-world', 'action-adventure'],
+  },
+
+  // ...
+]
+```;
+const gameOfTheYear = [
+	{
+		name: "Sekiro: Shadows die twice",
+		creator: ["Activision", "FromSoftware"],
+		releaseYear: 2019,
+		picture: "link/to/a/picture",
+		genre: ["Crime", "Drama"],
+	},
+
+	// ...
+]```
+const gameOfTheYear = [
+  {
+    name: 'God of War from',
+    creator: 'Sony Santa Monica ',
+    releaseYear: 2018,
+    picture: 'link/to/a/picture',
+    genre: ['action-adventure'],
+  },
+
+  // ...
+]
+```;
+const gameOfTheYear = [
+	{
+		name: "The Legend of Zelda: Breath of the Wild",
+		creator: "Carol Shaw",
+		releaseYear: 2017,
+		picture: "link/to/a/picture",
+		genre: ["action-adventure"],
+	},
+
+	// ...
+]```
+const gameOfTheYear = [
+  {
+    name: 'Overwatch',
+    creator: 'Blizzard Entertainment',
+    releaseYear: 2016,
+    picture: 'link/to/a/picture',
+    genre: ['team-based action game'],
+  },
+
+  // ...
+]
+```;
+const gameOfTheYear = [
+	{
+		name: "The Witcher 3: Wild Hunt",
+		creator: "CD PROJEKT",
+		releaseYear: 2015,
+		picture: "link/to/a/picture",
+		genre: ["single-player", "RPG"],
+	},
+
+	// ...
+]```
+const gameOfTheYear = [
+  {
+    name: 'Dragon Age: inquisition',
+    creator: 'Electronic Arts',
+    releaseYear: 1994,
+    picture: 'link/to/a/picture',
+    genre: ['action', 'RPG'],
+  },
+
+  // ...
+]
+```;
+const gameOfTheYear = [
+	{
+		name: "The last of us",
+		creator: "Naughty Dog",
+		releaseYear: 2013,
+		picture: "link/to/a/picture",
+		genre: ["action-adventure"],
+	},
+
+	// ...
+]```
+const gameOfTheYear = [
+  {
+    name: ' The Walking Dead',
+    creator: 'TellTaleGames',
+    releaseYear: 2012,
+    picture: 'link/to/a/picture',
+    genre: ['Graphic-adventure', 'Interactive Drama'],
+  },
+
+  // ...
+]
+```;
