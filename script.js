33a97fe15d1ab5b1b161cabba866c83cb2fb9966
Nicolas <93992407+Cloesick@users.noreply.github.com>
2022-01-13 10:05:20 +0100

    creating the structure of html with js

--- script.js
+++ script.js
@@ -1,115 +1,118 @@
+const SURVIVAL_HORROR = "survival-horor";
+const OPEN_WORLD = "open-world";
+const ACTION_ADVENTURE = "action-adventure";
+const GRAPHIC_ADVENTURE = "graphic-adventure";
+const SINGLE_PLAYER = "single-player";
+const INTERACTIVE_DRAMA = "interactive-drama";
+
 const gameOfTheYear = [
 	{
-		name: "Resident Evil Village",
+		game: "Resident Evil Village",
 		creator: "Quentin Tarantino",
 		releaseYear: 2021,
-		picture: "link/to/a/picture",
+		cover: "link/to/a/cover",
 		genre: ["survival-horror"],
 	},
 
-	// ...
-]```
-const gameOfTheYear = [
-  {
-    name: 'Cyberpunk 2077',
-    creator: 'CD Projekt',
-    releaseYear: 2020,
-    picture: 'link/to/a/picture',
-    genre: ['open-world', 'action-adventure'],
-  },
-
-  // ...
-]
-```;
-const gameOfTheYear = [
 	{
-		name: "Sekiro: Shadows die twice",
-		creator: ["Activision", "FromSoftware"],
-		releaseYear: 2019,
-		picture: "link/to/a/picture",
-		genre: ["Crime", "Drama"],
+		game: "Cyberpunk 2077",
+		creator: "CD Projekt",
+		releaseYear: 2020,
+		cover: "link/to/a/cover",
+		genre: ["open-world", "action-adventure"],
 	},
 
-	// ...
-]```
-const gameOfTheYear = [
-  {
-    name: 'God of War from',
-    creator: 'Sony Santa Monica ',
-    releaseYear: 2018,
-    picture: 'link/to/a/picture',
-    genre: ['action-adventure'],
-  },
-
-  // ...
-]
-```;
-const gameOfTheYear = [
 	{
-		name: "The Legend of Zelda: Breath of the Wild",
-		creator: "Carol Shaw",
-		releaseYear: 2017,
-		picture: "link/to/a/picture",
+		game: "Sekiro: Shadows die twice",
+		creator: ["Activision", "FromSoftware"],
+		releaseYear: 2019,
+		cover: "link/to/a/cover",
 		genre: ["action-adventure"],
 	},
 
-	// ...
-]```
-const gameOfTheYear = [
-  {
-    name: 'Overwatch',
-    creator: 'Blizzard Entertainment',
-    releaseYear: 2016,
-    picture: 'link/to/a/picture',
-    genre: ['team-based action game'],
-  },
-
-  // ...
-]
-```;
-const gameOfTheYear = [
 	{
-		name: "The Witcher 3: Wild Hunt",
+		game: "God of War from",
+		creator: "Sony Santa Monica ",
+		releaseYear: 2018,
+		cover: "link/to/a/cover",
+		genre: ["action-adventure"],
+	},
+
+	{
+		game: "The Legend of Zelda: Breath of the Wild",
+		creator: "Nintendo Entertainment",
+		releaseYear: 2017,
+		cover: "link/to/a/cover",
+		genre: ["action-adventure"],
+	},
+
+	{
+		game: "Overwatch",
+		creator: "Blizzard Entertainment",
+		releaseYear: 2016,
+		cover: "link/to/a/cover",
+		genre: ["RPG "],
+	},
+
+	{
+		game: "The Witcher 3: Wild Hunt",
 		creator: "CD PROJEKT",
 		releaseYear: 2015,
-		picture: "link/to/a/picture",
+		cover: "link/to/a/cover",
 		genre: ["single-player", "RPG"],
 	},
 
-	// ...
-]```
-const gameOfTheYear = [
-  {
-    name: 'Dragon Age: inquisition',
-    creator: 'Electronic Arts',
-    releaseYear: 1994,
-    picture: 'link/to/a/picture',
-    genre: ['action', 'RPG'],
-  },
-
-  // ...
-]
-```;
-const gameOfTheYear = [
 	{
-		name: "The last of us",
+		game: "Dragon Age: inquisition",
+		creator: "Electronic Arts",
+		releaseYear: 1994,
+		cover: "link/to/a/cover",
+		genre: ["action-adventure", "RPG"],
+	},
+
+	{
+		game: "The last of us",
 		creator: "Naughty Dog",
 		releaseYear: 2013,
-		picture: "link/to/a/picture",
+		cover: "link/to/a/cover",
 		genre: ["action-adventure"],
 	},
 
-	// ...
-]```
-const gameOfTheYear = [
-  {
-    name: ' The Walking Dead',
-    creator: 'TellTaleGames',
-    releaseYear: 2012,
-    picture: 'link/to/a/picture',
-    genre: ['Graphic-adventure', 'Interactive Drama'],
-  },
+	{
+		game: " The Walking Dead",
+		creator: "TellTaleGames",
+		releaseYear: 2012,
+		cover: "link/to/a/cover",
+		genre: ["graphic-adventure", "interactive-drama"],
+	},
+];
 
-  // ...
-]
-```;
+const cardsContainerElement = document.querySelector(".cards-container";);
+// use this step to get your "game elements into divs"
+
+for(gameOfTheYear){
+	//create "game" with its properties
+	const newGameElement = document.createElement ("p");
+	newGameElement.classList.add("game");
+	newGameElement.textContent=game.game;
+
+	const newCreatorElement = document.createElement ("p");
+	newCreatorElement.classList.add("creator");
+	newCreatorElement.textContent=game.creator;
+	
+	const newReleaseYearElement = document.createElement ("p");
+	newReleaseYearElement.classList.add("releaseYear");
+	newReleaseYearElement.textContent=game.releaseYear;
+	
+	const newCoverElement = document.createElement ("p");
+	newCoverElement.classList.add("cover");
+	newCoverElement.textContent=game.cover;
+	
+	const newGenreElement = document.createElement ("p");
+	newGenreElement.classList.add("genre");
+	newGenreElement.textContent=game.genre;
+	
+	
+
+
+}
